4003e7ff91f8c79224d1107bd03b6f286ce59f46
Nhlalo <nhlalonkosi@gmail.com>
2025-12-22 14:06:00 +0200

    fix: import the useRef to the header

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Logo from "../../../assets/searchimage.png";
 import { Menu } from "lucide-react";
 import Styles from "./header.module.css";
