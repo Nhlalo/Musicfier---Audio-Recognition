8f63eb7891b60b0e7bab45861bfd606eafb8f3ea
Nhlalo <nhlalonkosi@gmail.com>
2025-12-23 14:20:03 +0200

    feature: set up hover status on the view chart button
    - Should display  "view chart" when hovered over
    - Should display featured artists on the chart when not hovered over

--- src/pages/home/components/globalchart/charts.jsx
+++ src/pages/home/components/globalchart/charts.jsx
@@ -1,8 +1,12 @@
+import React, { useState, useEffect, useRef } from "react";
 import Styles from "./charts.module.css";
 import artistImg from "../../../../assets/artistImg.jpg";
 import { ChevronRight } from "lucide-react";
 
 export default function Charts() {
+  const [hoverStatus, setHoverStatus] = useState(false);
+  const displayChartBTN = useRef(null);
+
   return (
     <section className={Styles.globalChart}>
       <div className={Styles.globalChartWrapper}>
@@ -20,6 +24,9 @@ export default function Charts() {
             aria-label="View the chart"
             type="button"
             className={Styles.viewChartBTN}
+            ref={displayChartBTN}
+            onMouseEnter={() => setHoverStatus(true)}
+            onMouseLeave={() => setHoverStatus(false)}
           >
             <img
               src={artistImg}
@@ -27,10 +34,27 @@ export default function Charts() {
               aria-hidden="true"
               className={Styles.artistImg}
             />
-            <p className={Styles.top50}>GLOBAL TOP 50 CHART</p>
-            <p className={Styles.featuredArtists}>
+            <p
+              className={Styles.top50}
+              aria-hidden="true"
+              style={{ display: hoverStatus ? "none" : "block" }}
+            >
+              GLOBAL TOP 50 CHART
+            </p>
+            <p
+              className={Styles.featuredArtists}
+              aria-hidden="true"
+              style={{ display: hoverStatus ? "none" : "block" }}
+            >
               Featuring songs from Tyla, Drake and more
             </p>
+            <div
+              className={Styles.viewChart}
+              aria-hidden="true"
+              style={{ display: hoverStatus ? "flex" : "none" }}
+            >
+              View Chart
+            </div>
           </button>
         </div>
         <div className={Styles.top50SongsContainer}>
@@ -44,7 +68,7 @@ export default function Charts() {
               SEE ALL <ChevronRight aria-hidden="true" />
             </button>
           </div>
-          <hr className={Styles.mainHr} />
+          <hr className={Styles.mainHr} aria-hidden="true" />
         </div>
       </div>
     </section>
