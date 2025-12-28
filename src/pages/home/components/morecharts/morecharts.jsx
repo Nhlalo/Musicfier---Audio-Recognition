0448a127448335c8626c7e95d5ece4317f69e50f
Nhlalo <nhlalonkosi@gmail.com>
2025-12-28 17:50:19 +0200

    feature: structure the moreCharts component

---
+++ src/pages/home/components/morecharts/morecharts.jsx
@@ -0,0 +1,50 @@
+import { SectionIcon } from "lucide-react";
+import Styles from "./morecharts.module.css";
+import artistImg from "../../../../assets/artistImg.jpg";
+
+function MoreCharts({ location = "South Africa" }) {
+  const heading = `Top 50 ${location}`;
+  return (
+    <a
+      className={Styles.chartLink}
+      aria-label={`View the Top 50 ${location} chart`}
+      href="google.com"
+    >
+      <div className={Styles.featuredSongCoverContainer} aria-hidden="true">
+        <div className={Styles.featuredSongCoverWrapper}>
+          <img
+            src={artistImg}
+            alt="featured song cover"
+            className={Styles.featuredSongCover}
+          />
+        </div>
+        <div className={Styles.featuredSongCoverWrapper}>
+          <img
+            src={artistImg}
+            alt="featured song cover"
+            className={Styles.featuredSongCover}
+          />
+        </div>
+        <div className={Styles.featuredSongCoverWrapper}>
+          <img
+            src={artistImg}
+            alt="featured song cover"
+            className={Styles.featuredSongCover}
+          />
+        </div>
+      </div>
+      <div className={Styles.chartDescrContainer} aria-hidden="true">
+        <div className={Styles.chartDescrWrapper}>
+          <span className={Styles.chartDescrHeading}>Top 50</span>
+          <br />
+          <span className={Styles.chartDescrHeading}>{location}</span>
+          <p className={Styles.featuredArtists}>
+            Featuring songs from Muzik Loova, Sam Deep, Nia Pearl & Boohle, DJ
+            Smallz, ZinedinexSguche & 031choppa and more
+          </p>
+        </div>
+        <div className={Styles.viewMoreBTN}>VIEW</div>
+      </div>
+    </a>
+  );
+}
