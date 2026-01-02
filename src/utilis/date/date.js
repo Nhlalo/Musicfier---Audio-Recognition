5b15a8e7ed9a4f56398d48b64e918bfb5f2b7306
Nhlalo <nhlalonkosi@gmail.com>
2026-01-02 15:39:54 +0200

    feature: implement getting tomorrow's date

--- src/utilis/date/date.js
+++ src/utilis/date/date.js
@@ -4,3 +4,10 @@ function getTodayDate() {
 
   return today;
 }
+
+function getTomorrowDate() {
+  const today = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  const tomorrow = tomorrow.toISOString().split("T")[0]; // "2024-01-16"
+  return tomorrow;
+}
