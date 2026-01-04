be7b98e26fdf8d2be03020058de9c251eefff8d7
Nhlalo <nhlalonkosi@gmail.com>
2026-01-04 19:36:57 +0200

    feature: trap the focus within the sidebar
    -Use dialog instead of the aside
    -The dialog makes the implementation of a sidebar much easier
    -Define the elements that must be within the tabindex

--- src/pages/concerts/components/sidebar/sidebar.jsx
+++ src/pages/concerts/components/sidebar/sidebar.jsx
@@ -1,7 +1,18 @@
 import Styles from "./sidebar.module.css";
 import { ChevronDown, MapPinCheck, Calendar, X, Search } from "lucide-react";
+import { useState, useEffect, useRef, forwardRef } from "react";
+import { openDialog, closeDialog } from "../../../../utilis/side bar/sidebar";
 
-function Duration({ startDate = "2024-01-02", endDate = "2025-01-04" }) {
+let focusableElements;
+const talk = "class";
+
+/* Deal with focusable Elements, place in an array*/
+function Duration({
+  startDate = "2024-01-02",
+  endDate = "2025-01-04",
+  startDateInputRef,
+  endDateInputRef,
+}) {
   return (
     <div className={Styles.dateContainer}>
       <div className={Styles.startDateContainer}>
@@ -13,7 +24,8 @@ function Duration({ startDate = "2024-01-02", endDate = "2025-01-04" }) {
           id="startDate"
           name="start_date"
           className={Styles.startDate}
-          value={startDate}
+          defaultValue={startDate}
+          ref={startDateInputRef}
         />
       </div>
       <div className={Styles.endDateContainer}>
@@ -25,74 +37,134 @@ function Duration({ startDate = "2024-01-02", endDate = "2025-01-04" }) {
           id="endDate"
           name="end_date"
           className={Styles.endDate}
-          value={endDate}
+          defaultValue={endDate}
+          ref={endDateInputRef}
         />
       </div>
     </div>
   );
 }
-export default function Sidebar({ location = "South Africa" }) {
+const Sidebarby = forwardRef(function (props, ref) {
+  const { location, showBTNRef } = props; // Destructure from props
+  const upcomingBTNRef = useRef(null);
+  const todayBTNRef = useRef(null);
+  const tommorrowBTNRef = useRef(null);
+  const weekendBTNRef = useRef(null);
+  const customRangeBTNRef = useRef(null);
+  const startBTNRef = useRef(null);
+  const endsBTNRef = useRef(null);
+  const clearBTNRef = useRef(null);
+  const nearMeBTNRef = useRef(null);
+  const firstCountryBTNRef = useRef(null);
+  const secondCountryBTNRef = useRef(null);
+  const thirdCountryRef = useRef(null);
+  const newLocationBTNRef = useRef(null);
+  const searchInputBTNRef = useRef(null);
+
+  const allFocusableElements = [
+    showBTNRef,
+    upcomingBTNRef.current,
+    todayBTNRef.current,
+    tommorrowBTNRef.current,
+    weekendBTNRef.current,
+    customRangeBTNRef.current,
+    startBTNRef.current,
+    endsBTNRef.current,
+    clearBTNRef.current,
+    nearMeBTNRef.current,
+    firstCountryBTNRef.current,
+    secondCountryBTNRef.current,
+    thirdCountryRef.current,
+    newLocationBTNRef.current,
+    searchInputBTNRef.current,
+  ];
+  focusableElements = allFocusableElements;
+
   return (
-    <aside className={Styles.filterConcerts}>
-      <div className={Styles.headingContainer}>
-        <h2 className={Styles.heading}>Filter Concerts</h2>
-        <button
-          type="button"
-          className={Styles.hideBTN}
-          aria-label="Close the side bar"
-        >
-          <X aria-hidden="true" />
-        </button>
-      </div>
+    <dialog
+      className={`${Styles.filterConcerts} ${Styles.sidebarContainer}`}
+      ref={ref}
+    >
       <div className={Styles.whenContainer}>
-        <span class={Styles.visuallyHidden}>
+        <span className={Styles.visuallyHidden}>
           Select the date of the concerts
         </span>
         <h3 className={Styles.when} aria-hidden="true">
           When?
         </h3>
         <div className={Styles.todayContainer}>
-          <button type="button" className={`${Styles.upcoming} ${Styles.BTNs}`}>
+          <button
+            type="button"
+            className={`${Styles.upcoming} ${Styles.BTNs}`}
+            ref={upcomingBTNRef}
+          >
             All Upcoming
           </button>
-          <button type="button" className={`${Styles.today} ${Styles.BTNs}`}>
+          <button
+            type="button"
+            className={`${Styles.today} ${Styles.BTNs}`}
+            ref={todayBTNRef}
+          >
             Today
           </button>
         </div>
         <div className={Styles.tomorrowContainer}>
-          <button type="button" className={`${Styles.tomorrow} ${Styles.BTNs}`}>
+          <button
+            type="button"
+            className={`${Styles.tomorrow} ${Styles.BTNs}`}
+            ref={tommorrowBTNRef}
+          >
             Tomorrow
           </button>
-          <button type="button" className={`${Styles.weekend} ${Styles.BTNs}`}>
+          <button
+            type="button"
+            className={`${Styles.weekend} ${Styles.BTNs}`}
+            ref={weekendBTNRef}
+          >
             This Weekend
           </button>
         </div>
-        <button type="button" className={Styles.customRangeBTN}>
+        <button
+          type="button"
+          className={Styles.customRangeBTN}
+          ref={customRangeBTNRef}
+        >
           <Calendar aria-hidden="true" />{" "}
           <span className={Styles.customRange}>Custom Range</span>{" "}
           <ChevronDown aria-hidden="true" />
         </button>
-        <Duration />
+        <Duration
+          startDateInputRef={startBTNRef}
+          endDateInputRef={endsBTNRef}
+        />
       </div>
       <div className={Styles.whereContainer}>
         <div>
           <div className={Styles.clearContainer}>
-            <h3 class={Styles.visuallyHidden}>
+            <h3 className={Styles.visuallyHidden}>
               Select the locatiion of the concerts
             </h3>
             <h3 className={Styles.where} aria-hidden="true">
               Where?
             </h3>
-            <button type="button" className={Styles.clearBTN}>
+            <button type="button" className={Styles.clearBTN} ref={clearBTNRef}>
               clear
             </button>
           </div>
         </div>
         <div className={Styles.nearMeContainer}>
-          <button type="button" className={`${Styles.nearMe} ${Styles.BTNs}`}>
+          <button
+            type="button"
+            className={`${Styles.nearMe} ${Styles.BTNs}`}
+            ref={nearMeBTNRef}
+          >
             Near Me
           </button>
-          <button type="button" className={`${Styles.usa} ${Styles.BTNs}`}>
+          <button
+            type="button"
+            className={`${Styles.usa} ${Styles.BTNs}`}
+            ref={firstCountryBTNRef}
+          >
             USA
           </button>
         </div>
@@ -100,23 +172,98 @@ export default function Sidebar({ location = "South Africa" }) {
           <button
             type="button"
             className={`${Styles.australia} ${Styles.BTNs}`}
+            ref={secondCountryBTNRef}
           >
             Australia
           </button>
-          <button type="button" className={`${Styles.location} ${Styles.BTNs}`}>
+          <button
+            type="button"
+            className={`${Styles.location} ${Styles.BTNs}`}
+            ref={thirdCountryRef}
+          >
             {location}
           </button>
         </div>
-        <button type="button" className={Styles.newLocationBTN}>
+        <button
+          type="button"
+          className={Styles.newLocationBTN}
+          ref={newLocationBTNRef}
+        >
           <MapPinCheck aria-hidden="true" />
           <span className={Styles.newLocation}>New Location</span>{" "}
           <ChevronDown aria-hidden="true" />
         </button>
         <div className={Styles.countryInputContainer}>
           <Search className={Styles.searchIcon} />
-          <input type="text" name="country" className={Styles.countryInput} />
+          <input
+            type="text"
+            name="country"
+            className={Styles.countryInput}
+            ref={searchInputBTNRef}
+          />
         </div>
       </div>
-    </aside>
+    </dialog>
+  );
+});
+export default function SidebarVisibility() {
+  const [visibleButton, setVisibleButton] = useState("show");
+  const [buttonRef, setButtonRef] = useState(false);
+  const sidebarRef = useRef(null);
+  const showBTNRef = useRef(null);
+  const previousFocusedElement = useRef(null);
+
+  useEffect(() => {
+    setButtonRef(true);
+  }, []);
+  console.log(showBTNRef.current);
+
+  const handleCloseModal = function () {
+    const sideBarvalue = sidebarRef.current;
+    closeDialog(
+      sideBarvalue,
+      focusableElements,
+      previousFocusedElement.current,
+    );
+    setVisibleButton("show");
+  };
+  const handleOpenModal = function () {
+    console.log(sidebarRef.current);
+    previousFocusedElement.current = document.activeElement;
+    const sideBarvalue = sidebarRef.current;
+    openDialog(sideBarvalue, focusableElements);
+    setVisibleButton("hide");
+  };
+
+  return (
+    <>
+      <div className={Styles.filterConcerts}>
+        <div className={Styles.headingContainer}>
+          <h2 className={Styles.heading}>Filter Concerts</h2>
+          <button
+            type="button"
+            className={
+              visibleButton == "show" ? Styles.showBTN : Styles.noVisibility
+            }
+            aria-label="Open the side bar"
+            onClick={handleOpenModal}
+          >
+            show
+          </button>
+          <button
+            type="button"
+            className={
+              visibleButton == "hide" ? Styles.hideBTN : Styles.noVisibility
+            }
+            aria-label="Close the side bar"
+            onClick={handleCloseModal}
+            ref={showBTNRef}
+          >
+            <X aria-hidden="true" />
+          </button>
+        </div>
+      </div>
+      <Sidebarby ref={sidebarRef} showBTNRef={showBTNRef.current} />
+    </>
   );
 }
