b057d6ff6642008a1a4adecf3471ac4e478446cd
Nhlalo <nhlalonkosi@gmail.com>
2026-01-07 12:32:57 +0200

    feature: replace geonames library with API
    -There is no easy way to implement AbortController() with the library

--- src/utilis/country/country.js
+++ src/utilis/country/country.js
@@ -1,25 +1,34 @@
-import Geonames from "geonames.js";
+const GEONAMES_USERNAME = "TheBoy";
 
-const geonames = new Geonames({
-  username: "TheBoy", // Replace with your username
-  lan: "en",
-});
+//This aids in searching for the user's desired location by using the geonames api
+export default async function getLocation(location, signal) {
+  if (!location || location.trim() === "") {
+    return;
+  }
 
-async function getLocation(location) {
   try {
-    const response = await geonames.search({
-      q: location,
-      style: "SHORT",
-      maxRows: 6,
-    });
+    const query = encodeURIComponent(location.trim());
 
-    //The values are transformed in to a Country, city (London, United Kingdom) format
-    const locationResult = response.geonames.map((place) => {
-      return `${place.name}, ${place.countryName}`;
-    });
+    //This will return a maximum of 6 results with minimalistic data because style="short" is used
+    const url = `http://api.geonames.org/searchJSON?q=${query}&maxRows=6&username=${GEONAMES_USERNAME}&style=MEDIUM`;
+
+    const response = await fetch(url, { signal });
+
+    const data = await response.json();
+    console.log(data.geonames);
+    //Throw an error if the place does not exist within the Geonames database
+    if (!data.geonames || data.geonames.length === 0) {
+      throw new Error("No place found");
+    }
+
+    const locationResult = data.geonames.map((place) => ({
+      city: place.name,
+      country: place.countryName,
+      key: crypto.randomUUID(), // Keys are needed for any list creation
+    }));
 
     return locationResult;
   } catch (error) {
-    return [];
+    return;
   }
 }
