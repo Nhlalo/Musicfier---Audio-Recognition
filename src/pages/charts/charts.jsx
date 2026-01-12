0d3a649ab374dc1d30ee4e170ed8bd0fe061e8ad
Nhlalo <nhlalonkosi@gmail.com>
2026-01-12 14:41:23 +0200

    feature: implement the ChartList component

---
+++ src/pages/charts/charts.jsx
@@ -0,0 +1,11 @@
+import ChartHeader from "./components/chartheader/chartheader";
+import Footer from "../../components/layout/footer/footer";
+
+export default function ChartList() {
+  return (
+    <>
+      <ChartHeader />
+      <Footer />
+    </>
+  );
+}
