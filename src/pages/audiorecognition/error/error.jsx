183a000396c43add3b9303ad8e1e6af362e6b020
Nhlalo <nhlalonkosi@gmail.com>
2026-01-13 22:45:05 +0200

    feature: implement close pop up button handler
    -This handler will close the audio recognition error message

--- src/pages/audiorecognition/error/error.jsx
+++ src/pages/audiorecognition/error/error.jsx
@@ -1,10 +1,15 @@
 import { X } from "lucide-react";
 import Styles from "./error.module.css";
-export default function Error() {
+export default function Error({ error }) {
+  //This make the error message disappear by turning the error state in the About component to false
+  function handleClick() {
+    error(false);
+  }
+
   return (
     <div className={Styles.errorfeedback} role="status" aria-live="polite">
       <div className={Styles.errorfeedbackWrapper}>
-        <button type="button" className={Styles.closeBTN}>
+        <button type="button" className={Styles.closeBTN} onClick={handleClick}>
           {" "}
           <X className={Styles.xIcon} aria-hidden="true" />
         </button>
